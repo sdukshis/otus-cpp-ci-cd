d3322b456e08d51f017f367bc33e19739cb4ff24
Pavel Filonov <Pavel.Filonov@kaspersky.com>
2021-08-04 10:50:13 +0300

    exclude test_package fom clang-tidy checks

--- static-analysis.sh
+++ static-analysis.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR="$(dirname $0)"
 
 find "$SCRIPT_DIR" -not -path "*build/*" -and \
                    -not -path "*test_package/*" -and \
+                   -not -path "*tests/*" -and \
                    \( -name "*.h" -or -name "*.cpp" \) -print0 | \
 xargs -0 $CLANG_TIDY $@
 
