9193c61a972eab84a2c884122ba18238f05a90bb
Pavel Filonov <Pavel.Filonov@kaspersky.com>
2021-08-04 10:32:12 +0300

    add clang-tidy

---
+++ static-analisys.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+CLANG_TIDY="clang-tidy"
+SCRIPT_DIR="$(dirname $0)"
+
+find "$SCRIPT_DIR" -not -path "*build/*" -and \( -name "*.h" -or -name "*.cpp" \) -print0 | \
+xargs -0 $CLANG_TIDY $@
+
