55799b3ff5da8c83d49fea885c874bff109c4abd
Pavel Filonov <Pavel.Filonov@kaspersky.com>
2021-08-04 09:51:23 +0300

    clang-format added

---
+++ format.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+SCRIPT_DIR="$(dirname $0)"
+OPTS="-i"
+
+if [[ "$1" == "check" ]]; then
+    OPTS="--dry-run"
+fi
+
+find "$SCRIPT_DIR" -not -path "*build/*" -and \( -name "*.h" -or -name "*.cpp" \) -print0 | \
+xargs -0 clang-format "$OPTS"
+
--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,4 @@
 
 #include "lib/hello.h"
 
-int main() {
-    hello::greetings(std::cout);
-}
+int main() { hello::greetings(std::cout); }
