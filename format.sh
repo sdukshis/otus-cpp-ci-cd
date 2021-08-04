9850d61a723c132b393bc57190d6c8aa0bc3c9ac
Pavel Filonov <Pavel.Filonov@kaspersky.com>
2021-08-04 10:06:31 +0300

    add llvm-12 repository

--- format.sh
+++ format.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+CLANG_FORMAT="clang-format-12"
 SCRIPT_DIR="$(dirname $0)"
 OPTS="-i"
 
@@ -8,5 +9,5 @@ if [[ "$1" == "check" ]]; then
 fi
 
 find "$SCRIPT_DIR" -not -path "*build/*" -and \( -name "*.h" -or -name "*.cpp" \) -print0 | \
-xargs -0 clang-format --Wno-error=unknown "$OPTS"
+xargs -0 $CLANG_FORMAT --Wno-error=unknown "$OPTS"
 
